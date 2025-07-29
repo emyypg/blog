ac4d73b3d5d32c60e04716b9a77e4b6f377064f8
NeelBit <nelbertran@gmail.com>
2025-07-29 14:15:10 -0300

    template nosotros, carga de integrantes aleatorio.

--- core/views.py
+++ core/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+import random # ordenar aleatoriamente a los integrantes del equipo
 
 def home(request):
     return render(request, "home.html")
 
 def nosotros(request):
-    return render(request, "nosotros.html")
+    # ordenar aleatoriamente a los integrantes del equipo
+    integrantes = [
+        {'nombre': 'bertran, nelson', 'portfolio': 'https://portfolio-neelbit.netlify.app/', 'github': 'https://github.com/NeelBit'},
+        {'nombre': 'Cardozo, Ricardo', 'portfolio': '#', 'github': 'https://github.com/RickyRicardo19'},
+        {'nombre': 'Carlos, Eduardo Gomez', 'portfolio': '#', 'github': 'https://github.com/gcarloseduardo'},
+        {'nombre': 'Puente Gonzalez, Emily', 'portfolio': '#', 'github': 'https://github.com/emyypg'},
+        {'nombre': 'Velazco, Carlos Ariel', 'portfolio': '#', 'github': 'https://github.com/carvelaz'},
+    ]
+    
+    random.shuffle(integrantes)  # Mezclar la lista de integrantes
+    return render(request, 'nosotros.html', {'integrantes': integrantes})
+
+
