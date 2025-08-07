523fea788eaf5c6c55c3e69b69a93427125200ef
RickyRicardo19 <ricardoalfcardozo@gmail.com>
2025-08-06 22:06:54 -0300

    Te gustoo o no te gustooo

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('comentario', Comentar_Post, name='comentar'),
     path('<int:pk>/editar_comentario/', EditarComentario.as_view(), name='editar_cometario'),
     path('<int:pk>/eliminar_comentario/', CommentDeleteView.as_view(), name='eliminar_comentario'),
+    path('post/<int:post_id>/like/', views.like_post, name='like_post'),
+    path('comentario/<int:pk>/like/', views.like_comentario, name='like_comentario'),
 ]
