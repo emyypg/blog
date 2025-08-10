119c953a610364556e6104ea21a9f85ffef382de
Carlos Gomez <gcarloseduardo@hotmail.com>
2025-08-10 16:20:28 -0300

    grupo por defecto del usuario registrado

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import RegistroForm
+from django.contrib.auth.models import Group
 
 class Registro(CreateView):
     form_class = RegistroForm
@@ -10,5 +11,7 @@ class Registro(CreateView):
 
     def form_valid(self, form):
         response = super().form_valid(form)
+        group = Group.objects.get(name='visitante')
+        self.object.groups.add(group)
         login(self.request, self.object)  # Loguea automáticamente al usuario recién registrado
         return response
